13d67c5fca4631238cb81bfabe972c752724aea4
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-23 09:28:20 +0200

    Plusz feladatok longclick done

---
+++ app/src/main/java/com/example/pluszfeladatok/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.pluszfeladatok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView szam;
+    private Button plusz;
+    private Button minusz;
+
+    static int szamlalo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        szam.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                szamlalo = 0;
+                szam.setText("0");
+                szam.setTextColor(Color.rgb(0,0,255));
+                return false;
+            }
+        });
+        plusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo ++;
+                String pluszEgy = String.format("%s",szamlalo);
+                szam.setText(pluszEgy);
+                if (szamlalo == 0){
+                    szam.setTextColor(Color.BLUE);
+                }else if (szamlalo > 0){
+                    szam.setTextColor(Color.GREEN);
+                }
+            }
+        });
+        minusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo --;
+                String minuszEgy = String.format("%s",szamlalo);
+                szam.setText(minuszEgy);
+                if (szamlalo==0){
+                    szam.setTextColor(Color.BLUE);
+                }else if (szamlalo < 0){
+                    szam.setTextColor(Color.RED);
+                }
+
+            }
+        });
+    }
+    private void init(){
+        szam = findViewById(R.id.szam);
+        plusz = findViewById(R.id.plusz);
+        minusz = findViewById(R.id.minusz);
+    }
+}
